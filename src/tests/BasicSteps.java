aa6ebb07621091c72139eac3832ff06c78f92c71
Andrej Buday <andrej.buday@gmail.com>
2017-08-05 22:56:58 +0200

    Tests create testing file.

--- src/tests/BasicSteps.java
+++ src/tests/BasicSteps.java
@@ -3,6 +3,7 @@ package tests;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -10,17 +11,46 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import BasicProgram.Record;
+
 public class BasicSteps {
+	
+	private static String testFileName = "test.csv";
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		// CREATE EMPTY CSV FILE
-		File file = new File("testing.csv");
+		// CREATE NEW CSV FILE AND POPULATE IT WITH RANDOM DATA
+		File testFile = new File(testFileName);
+		ArrayList<Record> allRecords = new ArrayList<Record>();
+		Record newDailyRecord1 = new Record(2016, 01, 14, 4000, 70000); 
+		allRecords.add(newDailyRecord1);
+		Record newDailyRecord2 = new Record(2015, 05, 21, 5000, 45000);
+		allRecords.add(newDailyRecord2);
+		Record newDailyRecord3 = new Record(2010, 02, 21, 8000, 8000);
+		allRecords.add(newDailyRecord3);
+		Record newDailyRecord4 = new Record(2016, 07, 04, 4000, 9800);
+		allRecords.add(newDailyRecord4);
+		Record newDailyRecord5 = new Record(2014, 06, 1, 50, 14000);
+		allRecords.add(newDailyRecord5);
+		Record newDailyRecord6 = new Record(2013, 07, 11, 80, 81);
+		allRecords.add(newDailyRecord6);
+
+		for (Record record : allRecords) {
+			record.createRecordInDocument(testFileName);
+		}
+        
+        // TODO ASSERT COUNT NUMBERS OF ROWS
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		// DELETE EMPTY CSV FILE
+		// DELETE CSV FILE
+		File testFile = new File(testFileName);
+        if (testFile.delete()) {
+        	System.out.println("File deleted " + testFileName + " was deleted.");
+        } else {
+        	System.out.println("File was not deleted.");
+        }
 	}
 
 	@Before
--- src/tests/UnitTest.java
+++ src/tests/UnitTest.java
@@ -1,38 +1,55 @@
 package tests;
 
-import java.util.ArrayList;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import BasicProgram.Record;
 
 public class UnitTest {
+	
+	private static String testFileName = "test.csv";
 
 	@Before
 	public void setUp() throws Exception {
+		
+		// CREATE NEW CSV FILE AND POPULATE IT WITH RANDOM DATA
+		File testFile = new File(testFileName);
 		ArrayList<Record> allRecords = new ArrayList<Record>();
-		// noStepsPerDay, noCaloriesPerDay, recordYear, recordMonth, recordDay,
-		// recordID;
-		// TODO - Unit tests
-		// TEST
-		Record newDailyRecord1 = new Record(2016, 01, 14, 4000, 70000); // TEST
-		allRecords.add(newDailyRecord1); // TEST
-		Record newDailyRecord2 = new Record(2015, 05, 21, 5000, 45000); // TEST
-		allRecords.add(newDailyRecord2); // TEST
-		Record newDailyRecord3 = new Record(2010, 02, 21, 8000, 8000); // TEST
-		allRecords.add(newDailyRecord3); // TEST
-		Record newDailyRecord4 = new Record(2016, 07, 04, 4000, 9800); // TEST
-		allRecords.add(newDailyRecord4); // TEST
-		Record newDailyRecord5 = new Record(2014, 06, 1, 50, 14000); // TEST
-		allRecords.add(newDailyRecord5); // TEST
-		Record newDailyRecord6 = new Record(2013, 07, 11, 80, 81); // TEST
-		allRecords.add(newDailyRecord6); // TEST
+		Record newDailyRecord1 = new Record(2016, 01, 14, 4000, 70000); 
+		allRecords.add(newDailyRecord1);
+		Record newDailyRecord2 = new Record(2015, 05, 21, 5000, 45000);
+		allRecords.add(newDailyRecord2);
+		Record newDailyRecord3 = new Record(2010, 02, 21, 8000, 8000);
+		allRecords.add(newDailyRecord3);
+		Record newDailyRecord4 = new Record(2016, 07, 04, 4000, 9800);
+		allRecords.add(newDailyRecord4);
+		Record newDailyRecord5 = new Record(2014, 06, 1, 50, 14000);
+		allRecords.add(newDailyRecord5);
+		Record newDailyRecord6 = new Record(2013, 07, 11, 80, 81);
+		allRecords.add(newDailyRecord6);
 
 		for (Record record : allRecords) {
-			record.createRecordInDocument();
+			record.createRecordInDocument(testFileName);
 		}
+        
+        // TODO ASSERT COUNT NUMBERS OF ROWS
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+		// DELETE CSV FILE
+		File testFile = new File(testFileName);
+        if (testFile.delete()) {
+        	System.out.println("File deleted " + testFileName + " was deleted.");
+        } else {
+        	System.out.println("File was not deleted.");
+        }
 	}
 
 	@Test
